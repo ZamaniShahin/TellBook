b604b96490018d3a3d8b81a67aa3d8722e80eed3
ZamaniShahin <sh.zamani1581@gmail.com>
2023-07-24 12:46:44 +0330

    Add Contact Function Done

--- TellBook/Program.cs
+++ TellBook/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace TellBook
 {
@@ -10,6 +11,19 @@ namespace TellBook
     {
         static void Main(string[] args)
         {
+            WriteLine("1.Add");
+            WriteLine("2.Update");
+            WriteLine("3.Delete");
+            WriteLine("4.Search By Name:");
+            WriteLine("5.Search By LastName:");
+            WriteLine("6.Exit");
+            WriteLine("-------------------------");
+            Write("Please Choose An Option:");
+            string option = ReadLine();
+            while (true)
+            {
+
+            }
         }
     }
 }
