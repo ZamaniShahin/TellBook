0a451762c0a591981282c3332756a30aa7ab71ad
ZamaniShahin <sh.zamani1581@gmail.com>
2023-08-17 23:26:53 +0330

    Program.cs Updated

--- TellBook/Book.cs
+++ TellBook/Book.cs
@@ -6,10 +6,9 @@ public class Book
 {
     private readonly ContactDbContext _context;
 
-    public Book(ContactDbContext context, int numberOfContacts)
+    public Book(ContactDbContext context)
     {
         _context = context;
-        this.numberOfContacts = numberOfContacts;
     }
 
     public void Create(string firstName, string lastName, string phoneNumber, string emailAddress, string city)
@@ -19,83 +18,5 @@ public class Book
             );
 
         _context.Add(contact);
-    }
-
-    //public int numberOfContacts = 0;
-    //Contact[] tellBook = new Contact[40];
-    //private Contact CreateContact(ref Contact newContact, Contact oldContact)
-    //{
-
-    //    newContact.FirstName = oldContact.FirstName;
-    //    newContact.LastName = oldContact.LastName;
-    //    newContact.PhoneNumber = oldContact.PhoneNumber;
-    //    newContact.EmailAddress = oldContact.EmailAddress;
-    //    newContact.City = oldContact.City;
-    //    return newContact;
-    //}
-    private int SearchForEmptyIndex()
-    {
-        for (int i = 0; i < tellBook.Length; i++)
-        {
-            if (tellBook[i] == null)
-                return i;
-        }
-        return -1;
-    }
-    public void DisplayContact(int index)
-    {
-        Console.WriteLine($"FirstName:{tellBook[index].FirstName}");
-        Console.WriteLine($"LastName:{tellBook[index].LastName}");
-        Console.WriteLine($"EmailAddress:{tellBook[index].EmailAddress}");
-        Console.WriteLine($"City:{tellBook[index].City}");
-        Console.WriteLine($"PhoneNumber:{tellBook[index].PhoneNumber}");
-    }
-    public void AddContact(Contact newContact)
-    {
-        int emptyIndex = SearchForEmptyIndex();
-        if (tellBook[emptyIndex] == null)
-            tellBook[emptyIndex] = newContact;
-        CreateContact(ref tellBook[emptyIndex], newContact);
-        numberOfContacts++;
-    }
-    public void UpdateContact(Contact newContact, string phoneNumber)
-    {
-        int oldContactIndex = SearchByNumber(phoneNumber);
-        if (tellBook[oldContactIndex] != null && oldContactIndex != -1)
-        {
-            CreateContact(ref tellBook[oldContactIndex], newContact);
-        }
-        else
-            Console.WriteLine("There is No Contact With This Number.");
-    }
-    public void RemoveContact(string phoneNumber)
-    {
-        int theContactIndex = SearchByNumber(phoneNumber);
-        tellBook[theContactIndex] = null;
-        numberOfContacts--;
-    }
-    public int SearchByName(string firstName, string lastName)
-    {
-        for (int i = 0; i < tellBook.Length; i++)
-        {
-            if (tellBook[i].FirstName.ToLower() == firstName.ToLower()
-                && tellBook[i].LastName.ToLower() == lastName.ToLower()
-                && tellBook[i] != null)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-    public int SearchByNumber(string phoneNumber)
-    {
-        for (int i = 0; i < tellBook.Length; i++)
-        {
-            if (tellBook[i] != null && tellBook[i].PhoneNumber == phoneNumber)
-            {
-                return i;
-            }
-        }
-        return -1;
     }
 }
--- TellBook/Program.cs
+++ TellBook/Program.cs
@@ -10,7 +10,7 @@ namespace TellBook
     internal class Program
     {
         static void GetAndSetInfo(ref string firstName, ref string lastName, ref string city
-            , ref string phoneNumber, ref string email, ref Contact contact)
+            , ref string phoneNumber, ref string email)
         {
             WriteLine("Please Enter Contact's FirstName:");
             firstName = ReadLine();
@@ -22,18 +22,10 @@ namespace TellBook
             email = ReadLine();
             WriteLine("Please Enter Contact's PhoneNumber:");
             phoneNumber = ReadLine();
-
-            contact.FirstName = firstName;
-            contact.LastName = lastName;
-            contact.City = city;
-            contact.PhoneNumber = phoneNumber;
-            contact.EmailAddress = email;
         }
         static void Main(string[] args)
         {
             var dbContext = new ContactDbContext();
-
-            Contact contact = new Contact();
             Book tellBook = new Book();
             string oldPhoneNumber;
             string firstName, lastName, city, phoneNumber, email;
@@ -47,10 +39,8 @@ namespace TellBook
                 email = "";
                 oldPhoneNumber = "";
                 index = 0;
-                contact = null;
-                contact = new Contact();
                 Clear();
-                WriteLine($"You Have {tellBook.numberOfContacts} Contacts.");
+                WriteLine($"You Have {dbContext.Contacts.Count()} Contacts.");
                 WriteLine("-------------------------");
                 WriteLine("1.Add");
                 WriteLine("2.Update");
@@ -66,19 +56,19 @@ namespace TellBook
                 {
                     case 1:
                         GetAndSetInfo(ref firstName, ref lastName, ref city, ref phoneNumber
-                            , ref email, ref contact);
-                        tellBook.AddContact(contact);
+                            , ref email);
+                        tellBook.Create(firstName, lastName, phoneNumber, email, city);
                         break;
                     case 2:
                         WriteLine("Please Enter The Old Number Of Contact That You Want To Update.");
                         oldPhoneNumber = ReadLine();
-                        GetAndSetInfo(ref firstName, ref lastName, ref city, ref phoneNumber, ref email, ref contact);
-                        tellBook.UpdateContact(contact, oldPhoneNumber);
+                        GetAndSetInfo(ref firstName, ref lastName, ref city, ref phoneNumber, ref email);
+                        tellBook.Update(firstName, lastName, phoneNumber, email, city);
                         break;
                     case 3:
                         WriteLine("Please Enter The Old Number Of Contact That You Want To Delete.");
                         oldPhoneNumber = ReadLine();
-                        tellBook.RemoveContact(oldPhoneNumber);
+                        tellBook.Remove(oldPhoneNumber);
                         break;
                     case 4:
                         WriteLine("Please Enter Contact's FirstName:");
